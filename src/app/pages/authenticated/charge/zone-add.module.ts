750a6f35f15725cb628f1de6d6d3650d9c82d5f2
devkingkevcacharge <90602959+devkingkevcacharge@users.noreply.github.com>
2022-05-10 09:28:24 +0200

    Added German translation file which at the moment is only a copy of the English one.
    Added the AuthGuard thing which allows us to log in to the add-zone page from an iphone since it attempts to log in to app.cacharge.

--- src/app/pages/authenticated/charge/zone-add.module.ts
+++ src/app/pages/authenticated/charge/zone-add.module.ts
@@ -5,32 +5,33 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ZoneAddComponent } from './zone-add/zone-add.component';
-import { BraintreeFormComponentModule } from '../economy/braintree-form/braintree-form.component.module';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { DirectivesModule } from 'src/app/directives/directives.module';
 import { ZoneAddPage } from './zone-add.page';
 import { ZoneAddComponentModule } from './zone-add/zone-add.component.module';
+import { AuthGuard } from '../auth-guard.service';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: ZoneAddPage
-    }
+  {
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
+    path: '',
+    component: ZoneAddPage
+  }
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        RouterModule.forChild(routes),
-        ComponentsModule,
-        ZoneAddComponentModule,
-        DirectivesModule
-    ],
-    declarations: [ZoneAddPage],
-    entryComponents: []
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    ComponentsModule,
+    ZoneAddComponentModule,
+    DirectivesModule
+  ],
+  declarations: [ZoneAddPage],
+  entryComponents: []
 })
 export class ZoneAddPageModule { }
 
